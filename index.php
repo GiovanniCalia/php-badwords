cacff6dfc729a381ece8f0e14da80f1cfaef95ad
GiovanniCalia <johnca@outlook.it>
2022-04-07 15:29:13 +0200

    Stampaggio stringa e lunghezza

--- index.php
+++ index.php
@@ -7,8 +7,10 @@
   <title>PHP Badwords</title>
 </head>
 <body>
-  <?php $name = "Giovanni"
+  <?php
+   $nome = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   ?>
-  <h1>Ciao <?= $name ?></h1>
+  <h1> <?php echo $nome?></h1>
+  <h1>Lunghezza: <?php echo strlen($nome) ?></h1>
 </body>
 </html>
